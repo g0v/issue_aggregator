dc0b03dc024cea5f7c6e0970878d5e8de61d5d25
JmeHsieh <jmehsieh@gmail.com>
2016-07-10 13:19:51 +0800

    Git track data/ directory while ignoring all its contents.

--- download_jsons.py
+++ download_jsons.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import json
-import os
 import re
 import requests
 
@@ -30,11 +29,7 @@ def download_repos(session, repo_list_fpath, out_fpath):
         j = response.json()
         result.append(j)
 
-    try:
-        os.remove(out_fpath)
-    except OSError:
-        pass
-    with open(out_fpath, 'a+') as f:
+    with open(out_fpath, 'w+') as f:
         f.write(json.dumps(result))
 
     print('===== Repos Download Complete =====\n\n')
@@ -70,14 +65,9 @@ def download_issues(session, repo_json_fpath, out_issues_fpath, out_labels_fpath
                 matches = re.findall(r'\<(\S*)\>; rel="next"', response.headers['Link'])
                 page = matches[0] if len(matches) > 0 else None
 
-    try:
-        os.remove(out_issues_fpath)
-        os.remove(out_labels_fpath)
-    except OSError:
-        pass
-    with open(out_issues_fpath, 'a+') as f:
+    with open(out_issues_fpath, 'w+') as f:
         f.write(json.dumps(result))
-    with open(out_labels_fpath, 'a+') as f:
+    with open(out_labels_fpath, 'w+') as f:
         f.write(json.dumps(list(set(labels))))
 
     print('===== Download Issues Complete =====\n\n')
@@ -92,7 +82,6 @@ def download():
     s.headers.update({'Accept': 'application/vnd.github.v3+json'})
     s.headers.update({'Authorization': 'token %s' % authtoken})
 
-    os.makedirs('./data/', mode=0o755, exist_ok=True)
     download_repos(s, './data/url_list.json', './data/repos.json')
     download_issues(s, './data/repos.json', './data/issues.json', './data/labels.json')
 
